4ecbc9ffa90503a2ae31d721ae2c3945a87f2082
Dave Cassel <dmcassel@gmail.com>
2014-03-06 17:31:01 -0500

    removed a file

--- mlcp-enhanced-search-database-copy.sh
+++
@@ -1,6 +0,0 @@
-mlcp.sh copy -mode local \
-    -input_host localhost -input_port 8052 \
-    -thread_count 16 \
-    -input_username admin -input_password admin \
-    -output_host van-dev3.demo.marklogic.com -output_port 8052 \
-    -output_username mwhitehart -output_password welcome1
