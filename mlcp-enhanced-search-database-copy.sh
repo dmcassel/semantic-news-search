7328e1de06b2d5ff8a7487229772a7c2c7cb6c23
Dave Cassel <dmcassel@gmail.com>
2014-03-06 17:31:01 -0500

    removed a file

--- mlcp-enhanced-search-database-copy.sh
+++
@@ -1,6 +0,0 @@
-mlcp.sh copy -mode local \
-    -input_host localhost -input_port 8052 \
-    -thread_count 16 \
-    -input_username admin -input_password admin \
-    -output_host SERVER -output_port PORT \
-    -output_username USERNAME -output_password PASSWORD
