9c582dfc1026a74eef6c357adc0850099baedae7
Dave Cassel <dmcassel@gmail.com>
2014-08-28 07:37:04 -0400

    santizing

--- mlcp-enhanced-search-database-copy.sh
+++ mlcp-enhanced-search-database-copy.sh
@@ -2,5 +2,5 @@ mlcp.sh copy -mode local \
     -input_host localhost -input_port 8052 \
     -thread_count 16 \
     -input_username admin -input_password admin \
-    -output_host van-dev3.demo.marklogic.com -output_port 8052 \
-    -output_username mwhitehart -output_password welcome1
+    -output_host SERVER -output_port PORT \
+    -output_username USERNAME -output_password PASSWORD
